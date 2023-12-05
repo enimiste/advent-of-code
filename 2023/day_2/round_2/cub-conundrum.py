1ef5431a813d6eaa76b9175424df5360b96c7887
Enimiste <enimiste@users.noreply.github.com>
2023-12-05 15:25:37 +0100

    solution round 2

--- 2023/day_2/round_2/cub-conundrum.py
+++ 2023/day_2/round_2/cub-conundrum.py
@@ -34,6 +34,81 @@ def read_input() -> list[str]:
   from os import path
   base_dir = path.dirname(__file__)
   lines = []
-  with open(base_dir + '/round_1/input.txt', 'r') as inputFile:
+  with open(base_dir + '/input.txt', 'r') as inputFile:
     lines =  inputFile.readlines()
   return lines
+
+def parse_games(lines: list[str]) -> list[(int, set[(int, int, int)])] :
+  import re
+  pattern = r"^Game (\d+): (?(\d+) blue,)*;(?)$" #TODO
+  def parse_game(lineIdx: int, line: str) -> Union[Tuple[int, set[Tuple[int, int, int]]], None]:
+    def split_game_parts(line: str) -> Union[Tuple[str, set[str]], None]:
+      """
+      ex : Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+      """
+      if len(line)==0:
+        return None
+      parts = line.split(";")
+      if len(parts)<2:
+        return None
+      ps = parts[0].split(":")
+      if len(ps)<2:
+        return None
+      game = ps[0]#Game 1
+      sets = set([ps[1]] + parts[1:])
+      return (game, sets)
+
+    def extractGameId(game: str) -> int:
+      """
+      ex : Game 1
+      """
+      return int(game.split(" ")[1].strip())
+
+    def extractCubes(game_set: str) -> Tuple[int, int, int]:
+      """
+      ex :
+      8 green, 6 blue, 20 red
+      3 blue, 4 red
+      2 green
+      """
+      colors_idx = {"red": 0,"green": 1,"blue": 2}
+      colors = [0,0,0]
+      for color_str in game_set.split(","):
+        ps = color_str.strip().split(" ")
+        colors[colors_idx[ps[1].strip()]]=int(ps[0].strip())
+
+      return tuple(colors)
+
+    sp = split_game_parts(line)
+    if sp is None:
+      return None
+    game_id, game_sets = sp
+    return (extractGameId(game_id), {extractCubes(gs) for gs in game_sets})
+  
+  return [x for x in [parse_game(idx, line) for idx, line in enumerate(lines)] if x is not None]
+
+def power(games: list[(int, set[(int, int, int)])]) -> list[Tuple[int, int]]:
+  def prod(iter) -> int:
+    p = 1
+    for i in iter:
+      if i!=0:
+        p *= i
+    return p
+  def min_cubes_set(g_sets: set[(int, int, int)]) -> Tuple[int, int, int]:
+    maxs = [0,0,0]
+    for s in g_sets:
+      maxs[0] = max(maxs[0], s[0])
+      maxs[1] = max(maxs[1], s[1])
+      maxs[2] = max(maxs[2], s[2])
+    return maxs
+  
+  return [(id, prod(min_cubes_set(game_sets))) for (id, game_sets) in games]
+
+def power_over_all_games(powers: list[Tuple[int, int]]) -> int:
+  return sum([power for (_, power) in powers])
+
+if __name__=="__main__":
+  lines = example.splitlines()
+  games = parse_games(lines)
+  print(power_over_all_games(power(games)))
+
