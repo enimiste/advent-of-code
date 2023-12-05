502077659fd2b2e15a19f3a7f978dadbfe48c87f
Enimiste <enimiste@users.noreply.github.com>
2023-12-05 15:12:24 +0100

    Update cub-conundrum.py

--- 2023/day_2/round_2/cub-conundrum.py
+++ 2023/day_2/round_2/cub-conundrum.py
@@ -20,3 +20,20 @@ The power of a set of cubes is equal to the numbers of red, green, and blue cube
 
 For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
 """
+example = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
+
+from typing import Tuple, Union
+
+def read_input() -> list[str]:
+  from os import path
+  base_dir = path.dirname(__file__)
+  lines = []
+  with open(base_dir + '/round_1/input.txt', 'r') as inputFile:
+    lines =  inputFile.readlines()
+  return lines
