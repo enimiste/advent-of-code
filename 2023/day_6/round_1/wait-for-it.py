e5fedd955ca5634838989fd123e98233751a1ee5
Enimiste <nouni.elbachir@gmail.com>
2023-12-21 21:24:41 +0000

    Solution Day 6 round 1

--- 2023/day_6/round_1/wait-for-it.py
+++ 2023/day_6/round_1/wait-for-it.py
@@ -41,8 +41,8 @@ To see how much margin of error you have, determine the number of ways you can b
 
 Determine the number of ways you could beat the record in each race. What do you get if you multiply these numbers together?
 """
-example = """
-
+example = """Time:      7  15   30
+Distance:  9  40  200
 """
 
 
@@ -56,9 +56,39 @@ def read_input() -> list[str]:
     lines =  inputFile.readlines()
   return lines
 
-# TODO
+def parse_lines(lines: list[str]) -> list[Tuple[int, int]]:
+  if len(lines)!=2:
+    raise RuntimeError("There should be two lines max")
+  def extract_values_from_prefixed_line(line: str, prefix: str) -> list[int]:
+    return [int(x.strip()) for x in line.replace(prefix, "").strip().split(" ") if len(x.strip())>0]
+  
+  times = extract_values_from_prefixed_line(lines[0], "Time:")
+  distances = extract_values_from_prefixed_line(lines[1], "Distance:")
+
+  return list(zip(times, distances))
+
+def race_distance(time: int, distance_per_time: int) -> int:
+  return time * distance_per_time
+
+def count_race_ways(time: int, distance: int) -> int:
+  count = 0
+  for a in range(0, time+1):
+    d = race_distance(time-a, a)
+    if d>distance:
+      count+=1
+  return count
+
+def error_marge(races: list[Tuple[int, int]]) -> int:
+  ways_count = list()
+  for race in races:
+    ways_count.append(count_race_ways(race[0], race[1]))
+  
+  p = 1
+  for w in ways_count:
+    p*=w
+  return p
 
 if __name__=="__main__":
-  lines = example.splitlines()
-  #lines = read_input()
-  # TODO
+  #lines = example.splitlines()
+  lines = read_input()
+  print(error_marge(parse_lines(lines)))# 288 (example), 1413720 (input)
