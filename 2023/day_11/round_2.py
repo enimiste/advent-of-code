30ccf09bf1c784f660369bb2e42082f69d2d78e8
Enimiste <nouni.elbachir@gmail.com>
2023-12-21 21:51:55 +0000

    Create template folders for remaining days

---
+++ 2023/day_11/round_2.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+example = """
+"""
+
+
+from typing import Tuple, Union
+
+def read_input() -> list[str]:
+  from os import path
+  base_dir = path.dirname(__file__)
+  lines = []
+  with open(base_dir + '/input.txt', 'r') as inputFile:
+    lines =  inputFile.readlines()
+  return lines
+
+# TODO
+
+if __name__=="__main__":
+  lines = example.splitlines()
+  #lines = read_input()
+  print(___)# ___ (example), ___ (input)
